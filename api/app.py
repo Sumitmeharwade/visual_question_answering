cd8e606625c1f9effb50953ffed689f80759c365
01fe21bci032 <01fe21bci032@kletech.ac.in>
2024-04-25 16:51:11 +0530

    updated api

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
 origins = [
-    "http://localhost:3000",
+    "*",
 ]
 
 app.add_middleware(
