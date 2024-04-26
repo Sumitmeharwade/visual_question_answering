14ec5a9e45b0afe748b13398f1e9633c68e237c3
01fe21bci032 <01fe21bci032@kletech.ac.in>
2024-04-26 17:18:50 +0530

    added fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
