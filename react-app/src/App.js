76caa09b9efaf2cb0c8d7743be9caf990da99a60
01fe21bci032 <01fe21bci032@kletech.ac.in>
2024-05-14 00:02:32 +0530

    updated link

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
   // predict request handler
   async function handlePredict() {
     // API endpoint for prediction
-    const API_ENDPOINT = `http://localhost:8000/predict`;
+    const API_ENDPOINT = `https://visual-question-answering-vqa-api.vercel.app/predict`;
 
     // clear prediction
     setPrediction('...')
