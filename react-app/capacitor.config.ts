298157657f5e78c801fd9ade58e3173789c2fcd0
01fe21bci032 <01fe21bci032@kletech.ac.in>
2024-06-17 21:11:30 +0530

    updated home css

---
+++ react-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vqa.app',
+  appName: 'vqa-app',
+  webDir: 'build'
+};
+
+export default config;
